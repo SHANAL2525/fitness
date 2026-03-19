bc959e6d04c3df1270525755015b4452f9b51e3d
SHANAL2525 <s22010357@ousl.lk>
2026-03-19 23:31:40 +0530

    final one

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,18 +1,26 @@
-function Navbar() {
-  return (
-    <nav className="navbar">
-      <div className="navbar__logo">FITNESS</div>
+import React from "react";
+import "../../styles/navbar.css";
+import siteLogo from "../../assets/site-logo.png";
 
-      <ul className="navbar__links">
-        <li><a href="#hero">HOME</a></li>
-        <li><a href="#about">ABOUT</a></li>
-        <li><a href="#services">SERVICE</a></li>
-        <li><a href="#page">PAGE</a></li>
-        <li><a href="#blog">BLOG</a></li>
-        <li><a href="#contact">CONTACT</a></li>
-      </ul>
-    </nav>
+const Navbar = () => {
+  return (
+    <header className="navbar">
+      <div className="navbar-shell">
+        <div className="navbar-container">
+          <a href="#home" className="navbar-logo">
+            <img src={siteLogo} alt="Fitness Sports Center Logo" />
+          </a>
+
+          <nav className="navbar-menu">
+            <a href="#home">HOME</a>
+            <a href="#about">ABOUT</a>
+            <a href="#services">SERVICE</a>
+            <a href="#contact">CONTACT</a>
+          </nav>
+        </div>
+      </div>
+    </header>
   );
-}
+};
 
 export default Navbar;
